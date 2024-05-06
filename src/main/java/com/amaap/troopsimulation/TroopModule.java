b421fb99dee62a39d8f1e72b6e8af4a8c856c4a9
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-06 22:33:20 +0530

    Add Dependency Module

---
+++ src/main/java/com/amaap/troopsimulation/TroopModule.java
@@ -0,0 +1,22 @@
+package com.amaap.troopsimulation;
+import com.amaap.troopsimulation.repository.TrainedTroopRepository;
+import com.amaap.troopsimulation.repository.TroopRepository;
+import com.amaap.troopsimulation.repository.db.FakeDatabase;
+import com.amaap.troopsimulation.repository.db.impl.InMemoryFakeDatabase;
+import com.amaap.troopsimulation.repository.impl.InMemoryTrainedTrooperRepository;
+import com.amaap.troopsimulation.repository.impl.InMemoryTrooperRepository;
+import com.amaap.troopsimulation.service.BarrackService;
+import com.amaap.troopsimulation.service.TroopService;
+import com.google.inject.AbstractModule;
+
+public class TroopModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(FakeDatabase.class).to(InMemoryFakeDatabase.class);
+        bind(TroopRepository.class).to(InMemoryTrooperRepository.class);
+        bind(TrainedTroopRepository.class).to(InMemoryTrainedTrooperRepository.class);
+        bind(InMemoryTrooperRepository.class).toInstance(InMemoryTrooperRepository.getInstance(new InMemoryFakeDatabase()));
+        bind(TroopService.class);
+        bind(BarrackService.class);
+    }
+}
