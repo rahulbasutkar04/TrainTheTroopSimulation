715e23687b5127f6bd60b46abdb71eee40bb32b1
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-06 19:22:49 +0530

    Add barrack service

---
+++ src/main/java/com/amaap/troopsimulation/service/BarrackService.java
@@ -0,0 +1,27 @@
+package com.amaap.troopsimulation.service;
+
+import com.amaap.troopsimulation.domain.service.Train;
+import com.amaap.troopsimulation.repository.db.impl.InMemoryFakeDatabase;
+import com.amaap.troopsimulation.repository.impl.InMemoryTrainedTrooperRepository;
+import com.amaap.troopsimulation.repository.impl.InMemoryTrooperRepository;
+import com.amaap.troopsimulation.service.exception.InvalidTroopCountException;
+import java.util.List;
+
+public class BarrackService {
+    private final InMemoryTrooperRepository troopRepository;
+    private final InMemoryTrainedTrooperRepository inMemoryTrainedTrooperRepository;
+    private final Train train;
+
+    public BarrackService(InMemoryFakeDatabase fakeDatabase) {
+        this.troopRepository = InMemoryTrooperRepository.getInstance(fakeDatabase);
+        this.inMemoryTrainedTrooperRepository = new InMemoryTrainedTrooperRepository(fakeDatabase);
+        this.train = new Train(troopRepository, inMemoryTrainedTrooperRepository);
+    }
+    public boolean trainTroopers(List<Object> troopers) throws InvalidTroopCountException {
+        if (troopers.size() == 0) {
+            throw new InvalidTroopCountException("No Troopers Found To train...");
+        }
+
+        return train.trainTroopers();
+    }
+}
