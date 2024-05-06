ce2275dc41776a09b46e6321a71f51d314b600bc
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-06 13:22:08 +0530

    Add exception classes

---
+++ src/main/java/com/amaap/troopsimulation/service/exception/InvalidTroopCountException.java
@@ -0,0 +1,9 @@
+package com.amaap.troopsimulation.service.exception;
+
+public class InvalidTroopCountException extends Exception {
+    public InvalidTroopCountException(String message) {
+
+        super(message);
+
+    }
+}
---
+++ src/main/java/com/amaap/troopsimulation/service/exception/InvalidTroopTypeException.java
@@ -0,0 +1,8 @@
+
+package com.amaap.troopsimulation.service.exception;
+
+public class InvalidTroopTypeException extends Exception {
+    public InvalidTroopTypeException(String message) {
+        super(message);
+    }
+}
