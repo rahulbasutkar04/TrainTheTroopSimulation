be6ed5d7815214e651c6531cac310059e0cf7749
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-07 10:07:47 +0530

    Update BarackController class

--- src/main/java/com/amaap/troopsimulation/controller/BarrackController.java
+++ src/main/java/com/amaap/troopsimulation/controller/BarrackController.java
@@ -5,20 +5,28 @@ import com.amaap.troopsimulation.controller.dto.Response;
 import com.amaap.troopsimulation.repository.impl.InMemoryTrooperRepository;
 import com.amaap.troopsimulation.service.BarrackService;
 import com.amaap.troopsimulation.service.exception.InvalidTroopCountException;
+import com.google.inject.Inject;
 
 public class BarrackController {
     private final InMemoryTrooperRepository inMemoryTrooperRepository;
     private  final BarrackService barrackService;
 
-    public BarrackController(InMemoryTrooperRepository inMemoryTrooperRepository,BarrackService barrackService) {
-        this.inMemoryTrooperRepository=inMemoryTrooperRepository;
-        this.barrackService=barrackService;
+
+    @Inject
+    public BarrackController(InMemoryTrooperRepository inMemoryTrooperRepository, BarrackService barrackService) {
+        this.inMemoryTrooperRepository = inMemoryTrooperRepository;
+        this.barrackService = barrackService;
     }
 
-    public Response train() throws InvalidTroopCountException {
-       boolean isTrained= barrackService.trainTroopers(inMemoryTrooperRepository.getTroopers());
-        System.out.println(isTrained);
-        if(!isTrained) return  new Response(HttpStatus.BADREQUEST);
-        return new Response(HttpStatus.OK);
+    public Response train() {
+        try {
+            boolean isTrained = barrackService.trainTroopers(inMemoryTrooperRepository.getTroopers());
+            if (!isTrained) {
+                return new Response(HttpStatus.BADREQUEST);
+            }
+            return new Response(HttpStatus.OK);
+        } catch (InvalidTroopCountException e) {
+            return new Response(HttpStatus.BADREQUEST);
+        }
     }
 }
