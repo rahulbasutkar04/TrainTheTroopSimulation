94bd53db57a7ac79ffc97418ed4514d88ed84c2b
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-07 10:13:03 +0530

    Update inmemory trained troop repository

--- src/main/java/com/amaap/troopsimulation/repository/impl/InMemoryTrainedTrooperRepository.java
+++ src/main/java/com/amaap/troopsimulation/repository/impl/InMemoryTrainedTrooperRepository.java
@@ -2,20 +2,29 @@ package com.amaap.troopsimulation.repository.impl;
 
 import com.amaap.troopsimulation.repository.TrainedTroopRepository;
 import com.amaap.troopsimulation.repository.db.impl.InMemoryFakeDatabase;
+import com.google.inject.Inject;
 
 import java.util.List;
 
 public class InMemoryTrainedTrooperRepository implements TrainedTroopRepository {
     private final InMemoryFakeDatabase inMemoryFakeDatabase;
+    private static InMemoryTrainedTrooperRepository instance;
 
-    public InMemoryTrainedTrooperRepository(InMemoryFakeDatabase inMemoryFakeDatabase) {
+    @Inject
+    private InMemoryTrainedTrooperRepository(InMemoryFakeDatabase inMemoryFakeDatabase) {
         this.inMemoryFakeDatabase = inMemoryFakeDatabase;
+    }
+
+    public static InMemoryTrainedTrooperRepository getInstance(InMemoryFakeDatabase inMemoryFakeDatabase) {
+        if (instance == null) {
+            instance = new InMemoryTrainedTrooperRepository(inMemoryFakeDatabase);
+        }
+        return instance;
     }
 
     @Override
     public void insertToTrainedTrooperTable(Object trainedTrooper) {
         inMemoryFakeDatabase.insertIntoTrainedTrooperTable(trainedTrooper);
-
     }
 
     @Override
@@ -23,7 +32,25 @@ public class InMemoryTrainedTrooperRepository implements TrainedTroopRepository 
         return inMemoryFakeDatabase.getTrainedTroopers();
     }
 
-    public void clearTrainedTroopers() {
-        inMemoryFakeDatabase.getTroopers().clear();
+    @Override
+    public void setTrainedArcherCount(int archerCount) {
+        inMemoryFakeDatabase.setTrainedArcherCount(archerCount);
     }
+
+    @Override
+    public void setTrainedBarbarianCount(int barbarianCount) {
+        inMemoryFakeDatabase.setTrainedBarbarianCount(barbarianCount);
+    }
+
+    @Override
+    public int getTrainedArcherCount() {
+        return inMemoryFakeDatabase.getTrainedArcherCount();
+    }
+
+    @Override
+    public int getTrainedBarbarianCount() {
+        return inMemoryFakeDatabase.getTrainedBarbarianCount();
+    }
+
+
 }
