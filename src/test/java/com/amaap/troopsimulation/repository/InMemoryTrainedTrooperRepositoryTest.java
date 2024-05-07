440d9b8e56a71bbd8f06893ec073f4d62806ce08
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-07 10:03:01 +0530

    Add Inmemory Trained Trooper Repository class

---
+++ src/test/java/com/amaap/troopsimulation/repository/InMemoryTrainedTrooperRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.amaap.troopsimulation.repository;
+
+
+import com.amaap.troopsimulation.repository.db.impl.InMemoryFakeDatabase;
+import com.amaap.troopsimulation.repository.impl.InMemoryTrainedTrooperRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryTrainedTrooperRepositoryTest {
+
+    private InMemoryFakeDatabase fakeDatabase;
+    private InMemoryTrainedTrooperRepository trainedTrooperRepository;
+
+    @BeforeEach
+    void setUp() {
+        fakeDatabase = new InMemoryFakeDatabase();
+        trainedTrooperRepository = InMemoryTrainedTrooperRepository.getInstance(fakeDatabase);
+        fakeDatabase.clearDatabase();
+    }
+
+    @Test
+    void shouldBeAbleToInsertAndRetrieveTrainedTroopers() {
+        // arrange
+        Object trainedTrooper = new Object();
+
+        // act
+        trainedTrooperRepository.insertToTrainedTrooperTable(trainedTrooper);
+        var trainedTroopers = trainedTrooperRepository.getTrainedTroopers();
+
+        // assert
+        assertTrue(trainedTroopers.contains(trainedTrooper));
+    }
+
+    @Test
+    void shouldBeAbleToRetrieveTrainedArcherCount() {
+        // arrange
+        int expectedArcherCount = 5;
+        trainedTrooperRepository.setTrainedArcherCount(expectedArcherCount);
+
+        // act
+        int actualArcherCount = trainedTrooperRepository.getTrainedArcherCount();
+
+        // assert
+        assertEquals(expectedArcherCount, actualArcherCount);
+    }
+
+    @Test
+    void shouldBeAbleToRetrieveTrainedBarbarianCount() {
+        // arrange
+        int expectedBarbarianCount = 3;
+        trainedTrooperRepository.setTrainedBarbarianCount(expectedBarbarianCount);
+
+        // act
+        int actualBarbarianCount = trainedTrooperRepository.getTrainedBarbarianCount();
+
+        // assert
+        assertEquals(expectedBarbarianCount, actualBarbarianCount);
+    }
+
+
+
+}
