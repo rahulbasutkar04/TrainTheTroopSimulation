daf5a01cc7625b9591595146dbf3fc3bd1392225
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-07 10:10:29 +0530

    Update Repository

--- src/main/java/com/amaap/troopsimulation/repository/TrainedTroopRepository.java
+++ src/main/java/com/amaap/troopsimulation/repository/TrainedTroopRepository.java
@@ -1,5 +1,8 @@
 package com.amaap.troopsimulation.repository;
 
+import com.amaap.troopsimulation.domain.model.Archer;
+import com.amaap.troopsimulation.domain.model.Barbarian;
+
 import java.util.List;
 
 public interface TrainedTroopRepository {
@@ -7,4 +10,12 @@ public interface TrainedTroopRepository {
     void insertToTrainedTrooperTable(Object trainedTrooper);
 
     List<Object> getTrainedTroopers();
+
+
+    void setTrainedArcherCount(int archerCount);
+
+    void setTrainedBarbarianCount(int barbarianCount);
+
+    int getTrainedArcherCount();
+    int getTrainedBarbarianCount();
 }
